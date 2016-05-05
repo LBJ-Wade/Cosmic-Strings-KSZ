83fcdd2f43bb1ec03edeefcde5829db01ad9281f
gcanasherrera <guadalupecah@gmail.com>
2016-05-05 12:27:39 +0200

    add new class to calculate fourier transform

--- try.py
+++ try.py
@@ -1,12 +1,21 @@
-from Class_CosmicStrings import CosmicStrings
+from Class_CosmicStrings import CosmicStrings, PowerSpectrum
 import time
 
+print 'Starting Simulation'
+
+
 t = time.time()
-simulation=CosmicStrings(L=12.8, G_mu=1, n=15, gamma=1, v_bar=15)
-#simulation.cosmic_looper()
-simulation.cosmic_strings_creator(1)
+simulation=CosmicStrings(L=12.8, G_mu=6.0E-8, n=10, gamma=1, v_bar=15)
+simulation.cosmic_looper()
+#simulation.cosmic_strings_creator(1)
 elapsed = time.time() - t
 print elapsed
+
+
+fft=PowerSpectrum(simulation.M_field_view)
+fft.plotter_fft()
+
+
 
 
 
